902b3ec54c0209b81800ec23f1e18660d7f3dfaf
sammybee04 <sbsamboat9@icloud.com>
2020-10-16 06:08:27 -0500

    adding changes to repo

---
+++ build/precache-manifest.a13e73c2fca9d0009405523b7c387449.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a28cda985047e811f9bc2b0c4514e1b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b260c4dc0ff46ccd3cf0",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "171c05c29d1e17545e27",
+    "url": "/static/js/2.d7bc3e4c.chunk.js"
+  },
+  {
+    "revision": "92bcffc488d6f3fbd12a7720ee4a3b28",
+    "url": "/static/js/2.d7bc3e4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b260c4dc0ff46ccd3cf0",
+    "url": "/static/js/main.c5bb333f.chunk.js"
+  },
+  {
+    "revision": "e816113dc71f7db44c36",
+    "url": "/static/js/runtime-main.a660538a.js"
+  }
+]);
